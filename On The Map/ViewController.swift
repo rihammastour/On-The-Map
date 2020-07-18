a65b73b77202e8562aa581ff45b12030aa6ad688
rihammastour <48338432+rihammastour@users.noreply.github.com>
2020-07-18 07:31:56 +0300

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Riham Mastour on 27/11/1441 AH.
+//  Copyright © 1441 Riham Mastour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
